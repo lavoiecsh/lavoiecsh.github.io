274196afd71360e0ba6af4853eecdfb224022e19
Sébastien Lavoie-Courchesne <lavoiecsh@gmail.com>
2018-12-27 01:43:33 -0500

    Advent of Code: Day 22

--- advent/advent.solvers/Day23Solver.cs
+++ advent/advent.solvers/Day23Solver.cs
@@ -25,236 +25,30 @@ namespace advent.solvers
         public string SolveSecondPart()
         {
             var nanobots = dataProvider.GetData();
-            var minX = nanobots.Min(n => n.Position.X);
-            var minY = nanobots.Min(n => n.Position.Y);
-            var minZ = nanobots.Min(n => n.Position.Z);
-            var maxX = nanobots.Max(n => n.Position.X);
-            var maxY = nanobots.Max(n => n.Position.Y);
-            var maxZ = nanobots.Max(n => n.Position.Z);
-            var completeOctree = new OctreeBin(minX, minY, minZ, maxX, maxY, maxZ);
-            var validBins = completeOctree.Children();
-            while (validBins.Count != 1)
+
+            var queue = new List<(int, int)>();
+            foreach (var nanobot in nanobots)
             {
-                Console.WriteLine($"{validBins.Count}, First: {validBins.First()} with count = {validBins.First().CountInside(nanobots)}");
-                var counts = validBins.ToDictionary(b => b, b => b.CountInside(nanobots));
-                var maxCount = counts.Values.Max();
-                validBins = counts.Where(kv => kv.Value == maxCount)
-                    .SelectMany(kv => kv.Key.Children())
-                    .ToList();
+                var d = nanobot.DistanceToOrigin();
+                queue.Add((Math.Max(0, d - nanobot.Radius), 1));
+                queue.Add((d + nanobot.Radius + 1, -1));
             }
 
-            return validBins.Single().DistanceToOrigin().ToString();
-        }
-
-        private class OctreeBin
-        {
-            private readonly int minX;
-            private readonly int minY;
-            private readonly int minZ;
-            private readonly int maxX;
-            private readonly int maxY;
-            private readonly int maxZ;
-
-            public OctreeBin(int minX, int minY, int minZ, int maxX, int maxY, int maxZ)
+            var count = 0;
+            var maxCount = 0;
+            var result = 0;
+            queue = queue.OrderBy(kv => kv.Item1).ToList();
+            while (queue.Any())
             {
-                this.minX = minX;
-                this.minY = minY;
-                this.minZ = minZ;
-                this.maxX = maxX;
-                this.maxY = maxY;
-                this.maxZ = maxZ;
+                var (d, e) = queue.First();
+                queue.RemoveAt(0);
+                count += e;
+                if (count <= maxCount) continue;
+                result = d;
+                maxCount = count;
             }
 
-            internal int DistanceToOrigin()
-            {
-                return maxX + maxY + maxZ;
-            }
-
-            internal IList<OctreeBin> XChildren()
-            {
-                if (minX == maxX) return new List<OctreeBin> {this};
-                var halfX = (minX + maxX) / 2;
-                return new List<OctreeBin>
-                {
-                    new OctreeBin(minX, minY, minZ, halfX, maxY, maxZ),
-                    new OctreeBin(halfX + 1, minY, minZ, maxX, maxY, maxZ)
-                };
-            }
-
-            internal IList<OctreeBin> YChildren()
-            {
-                if (minY == maxY) return new List<OctreeBin>{this};
-                var halfY = (minY + maxY) / 2;
-                return new List<OctreeBin>
-                {
-                    new OctreeBin(minX, minY, minZ, maxX, halfY, maxZ),
-                    new OctreeBin(minX, halfY + 1, minZ, maxX, maxY, maxZ)
-                };
-            }
-
-            internal IList<OctreeBin> ZChildren()
-            {
-                if (minZ == maxZ) return new List<OctreeBin> {this};
-                var halfZ = (minZ + maxZ) / 2;
-                return new List<OctreeBin>
-                {
-                    new OctreeBin(minX, minY, minZ, maxX, maxY, halfZ),
-                    new OctreeBin(minX, minY, halfZ + 1, maxX, maxY, maxZ)
-                };
-            }
-
-            internal IList<OctreeBin> Children()
-            {
-                if (minX == maxX &&
-                    minY == maxY &&
-                    minZ == maxZ)
-                    return new List<OctreeBin> {this};
-
-                if (minX != maxX)
-                {
-                    var halfX = (minX + maxX) / 2;
-                    return new List<OctreeBin>
-                    {
-                        new OctreeBin(minX, minY, minZ, halfX, maxY, maxZ),
-                        new OctreeBin(halfX + 1, minY, minZ, maxX, maxY, maxZ)
-                    };
-                }
-
-                if (minY != maxY)
-                {
-                    var halfY = (minY + maxY) / 2;
-                    return new List<OctreeBin>
-                    {
-                        new OctreeBin(minX, minY, minZ, maxX, halfY, maxZ),
-                        new OctreeBin(minX, halfY + 1, minZ, maxX, maxY, maxZ)
-                    };
-                }
-
-                var halfZ = (minZ + maxZ) / 2;
-                return new List<OctreeBin>
-                {
-                    new OctreeBin(minX, minY, minZ, maxX, maxY, halfZ),
-                    new OctreeBin(minX, minY, halfZ + 1, maxX, maxY, maxZ)
-                };
-                
-//
-//                var halfX = (minX + maxX) / 2;
-//                var halfY = (minY + maxY) / 2;
-//                var halfZ = (minZ + maxZ) / 2;
-//
-//                if (minX == maxX)
-//                {
-//                    if (minY == maxY)
-//                        return new List<OctreeBin>
-//                        {
-//                            new OctreeBin(minX, minY, minZ, maxX, maxY, halfZ),
-//                            new OctreeBin(minX, minY, halfZ + 1, maxX, maxY, maxZ)
-//                        };
-//                    return new List<OctreeBin>
-//                    {
-//                        new OctreeBin(minX, minY, minZ, maxX, halfY, halfZ),
-//                        new OctreeBin(minX, minY, halfZ + 1, maxX, halfY, maxZ),
-//                        new OctreeBin(minX, halfY + 1, minZ, maxX, maxY, halfZ),
-//                        new OctreeBin(minX, halfY + 1, halfZ + 1, maxX, maxY, maxZ)
-//                    };
-//                }
-//
-//                if (minY == maxY)
-//                {
-//                    if (minZ == maxZ)
-//                    {
-//                        return new List<OctreeBin>
-//                        {
-//                            new OctreeBin(minX, minY, minZ, halfX, maxY, maxZ),
-//                            new OctreeBin(halfX + 1, minY, minZ, maxX, maxY, maxZ)
-//                        };
-//                    }
-//
-//                    return new List<OctreeBin>
-//                    {
-//                        new OctreeBin(minX, minY, minZ, halfX, maxY, halfZ),
-//                        new OctreeBin(minX, minY, halfZ + 1, halfX, maxY, maxZ),
-//                        new OctreeBin(halfX + 1, minY, minZ, maxX, maxY, halfZ),
-//                        new OctreeBin(halfX + 1, minY, halfZ + 1, maxX, maxY, maxZ)
-//                    };
-//                }
-//
-//                if (minZ == maxZ)
-//                {
-//                    return new List<OctreeBin>
-//                    {
-//                        new OctreeBin(minX, minY, minZ, halfX, halfY, maxZ),
-//                        new OctreeBin(minX, halfY + 1, minZ, halfX, maxY, maxZ),
-//                        new OctreeBin(halfX + 1, minY, minZ, maxX, halfY, maxZ),
-//                        new OctreeBin(halfX + 1, halfY + 1, minZ, maxX, maxY, maxZ)
-//                    };
-//                }
-//
-//                return new List<OctreeBin>
-//                {
-//                    new OctreeBin(minX, minY, minZ, halfX, halfY, halfZ),
-//                    new OctreeBin(minX, minY, halfZ + 1, halfX, halfY, maxZ),
-//                    new OctreeBin(minX, halfY + 1, minZ, halfX, maxY, halfZ),
-//                    new OctreeBin(minX, halfY + 1, halfZ + 1, halfX, maxY, maxZ),
-//                    new OctreeBin(halfX + 1, minY, minZ, maxX, halfY, halfZ),
-//                    new OctreeBin(halfX + 1, minY, halfZ + 1, maxX, halfY, maxZ),
-//                    new OctreeBin(halfX + 1, halfY + 1, minZ, maxX, maxY, halfZ),
-//                    new OctreeBin(halfX + 1, halfY + 1, halfZ + 1, maxX, maxY, maxZ)
-//                };
-            }
-
-            internal int CountInside(IEnumerable<Nanobot> nanobots)
-            {
-                return nanobots.Count(Intersects);
-            }
-
-            private bool Intersects(Nanobot nanobot)
-            {
-                var d = nanobot.Radius;
-                if (nanobot.Position.X < minX) d -= (minX - nanobot.Position.X);
-                if (nanobot.Position.X > maxX) d -= (nanobot.Position.X - maxX);
-                if (nanobot.Position.Y < minY) d -= (minY - nanobot.Position.Y);
-                if (nanobot.Position.Y > maxY) d -= (nanobot.Position.Y - maxY);
-                if (nanobot.Position.Z < minZ) d -= (minZ - nanobot.Position.Z);
-                if (nanobot.Position.Z > maxZ) d -= (nanobot.Position.Z - maxZ);
-                return d >= 0;
-            }
-
-            private bool Equals(OctreeBin other)
-            {
-                return minX == other.minX &&
-                       minY == other.minY &&
-                       minZ == other.minZ &&
-                       maxX == other.maxX &&
-                       maxY == other.maxY &&
-                       maxZ == other.maxZ;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (ReferenceEquals(null, obj)) return false;
-                if (ReferenceEquals(this, obj)) return true;
-                return obj.GetType() == GetType() && Equals((OctreeBin) obj);
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked
-                {
-                    var hashCode = minX;
-                    hashCode = (hashCode * 397) ^ minY;
-                    hashCode = (hashCode * 397) ^ minZ;
-                    hashCode = (hashCode * 397) ^ maxX;
-                    hashCode = (hashCode * 397) ^ maxY;
-                    hashCode = (hashCode * 397) ^ maxZ;
-                    return hashCode;
-                }
-            }
-
-            public override string ToString()
-            {
-                return $"{minX},{minY},{minZ} to {maxX},{maxY},{maxZ}";
-            }
+            return result.ToString();
         }
 
         public class Nanobot
@@ -283,6 +77,11 @@ namespace advent.solvers
             public override string ToString()
             {
                 return $"{Position.X} {Position.Y} {Position.Z} with radius {Radius}";
+            }
+
+            public int DistanceToOrigin()
+            {
+                return Math.Abs(Position.X) + Math.Abs(Position.Y) + Math.Abs(Position.Z);
             }
         }
     }
