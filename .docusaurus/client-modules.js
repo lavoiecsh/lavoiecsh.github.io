b9d3e8a8d67d5cad21cd08a4865b983a52eba40b
Sebastien Lavoie-Courchesne <sebastien.lavoie-courchesne@appdirect.com>
2023-03-26 17:42:20 -0400

    Template Docusaurus Website

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/sebastien.lavoie-cou/code/lavoiecsh/lavoiecsh.github.io/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/sebastien.lavoie-cou/code/lavoiecsh/lavoiecsh.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/sebastien.lavoie-cou/code/lavoiecsh/lavoiecsh.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/sebastien.lavoie-cou/code/lavoiecsh/lavoiecsh.github.io/src/css/custom.css'),
+];
